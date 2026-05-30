b0d52cea6ee84ad56d19b5e1c031b9d8e7dee4c4
andresbotello <acbotello@unicesar.edu.co>
2026-05-30 10:59:10 -0500

    Cambio de puertos en local

--- front-exposoftware/vite.config.js
+++ front-exposoftware/vite.config.js
@@ -5,9 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    port: 3001,
     proxy: {
       '/api': {
-        target: 'https://expounicesar.duckdns.org',
+        target: 'http://localhost:8000',
         changeOrigin: true,
         secure: false,
         cookieDomainRewrite: { '*': 'localhost' },
